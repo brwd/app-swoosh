d8860208021760d183cb3ca08aa2b91ea6deb68c
Payton Miller <7292029+paytonmiller@users.noreply.github.com>
2017-10-06 11:29:55 -0500

    Adding contraints

--- app-swoosh/app-swoosh/ViewController.swift
+++ app-swoosh/app-swoosh/ViewController.swift
@@ -17,10 +17,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        //centers the logo 50pts from top
-        swooshLogo.frame = CGRect(x: view.frame.size.width / 2 - swooshLogo.frame.size.width / 2, y: 50, width: swooshLogo.frame.size.width, height: swooshLogo.frame.size.height)
-        
-        bgImg.frame = view.frame;
+
     }
 
     override func didReceiveMemoryWarning() {
