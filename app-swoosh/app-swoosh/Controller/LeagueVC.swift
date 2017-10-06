65450ee643c67420fa830b724417d2177ca86f14
Payton Miller <7292029+paytonmiller@users.noreply.github.com>
2017-10-06 17:03:31 -0500

    created player model and added actions

--- app-swoosh/app-swoosh/Controller/LeagueVC.swift
+++ app-swoosh/app-swoosh/Controller/LeagueVC.swift
@@ -10,14 +10,38 @@ import UIKit
 
 class LeagueVC: UIViewController {
     
+    var player: Player!
+    
+    @IBOutlet weak var nextBtn: BorderButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        player = Player()
 
-        // Do any additional setup after loading the view.
     }
-    
+    // try to never have logic in IBActions, instead see function below
     @IBAction func userTappedNext(_ sender: Any) {
         performSegue(withIdentifier: "skillVCSegue", sender: self)
+    }
+    
+    @IBAction func onMensTapped(_ sender: Any) {
+        selectLeague(leagueType: "mens")
+    }
+    
+    @IBAction func onWomensTapped(_ sender: Any) {
+        selectLeague(leagueType: "womens")
+    }
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectLeague(leagueType: "coed")
+    }
+    
+    //function prevents from copying nextBtn.isEnabled = true on each IBAction
+    func selectLeague(leagueType: String) {
+        player.desiredLeague = leagueType
+        nextBtn.isEnabled = true
     }
     
 
